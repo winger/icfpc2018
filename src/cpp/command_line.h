72305e9872a7a48bbfa826dbe9e189778d65ffa8
Den Raskovalov <denbox@gmail.com>
2018-07-21 00:11:53 -0700

    unordered_map -> map

--- src/cpp/command_line.h
+++ src/cpp/command_line.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <unordered_map>
+#include <map>
 
 struct CommandLine {
     void Parse(int argc, char* const argv[]);
 
-    std::unordered_map<std::string, std::string> args;
-    std::unordered_map<std::string, int> int_args;
+    std::map<std::string, std::string> args;
+    std::map<std::string, int> int_args;
 };
 
 extern CommandLine cmd;
