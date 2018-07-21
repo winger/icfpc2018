e7b7c6c072e847bc20cbe5b8b794d0205f5dd6f0
alexey <alexey@oalexey2>
2018-07-20 20:21:52 -0700

    Use score similar to problem definition (with assumption that best solution has 0 energy).

--- src/cpp/solver.cpp
+++ src/cpp/solver.cpp
@@ -9,7 +9,7 @@ uint64_t Solver::Solve(const Matrix& m, Trace& output)
     return SolverLayersBase::Solve(m, output);
 }
 
-double Solver::Solve(unsigned model_index)
+unsigned Solver::Solve(unsigned model_index)
 {
     string si = to_string(1000 + model_index).substr(1);
     Matrix model;
@@ -22,16 +22,16 @@ double Solver::Solve(unsigned model_index)
     trace_dflt.ReadFromFile("dfltTracesL/LA" + si + ".nbt");
     uint64_t energy3 = Evaluation::CheckSolution(model, trace_dflt);
     double performance = ((energy2 >= energy3) || (energy2 == 0)) ? 0 : (1.0 - double(energy2) / double(energy3));
+    unsigned score = unsigned(1000.0 * performance * unsigned(log(model.GetR()) / log(2)));
     cout << "Test " << si << ": " << performance << endl;
     trace.WriteToFile("cppTracesL/LA" + si + ".nbt");
-    return performance;
+    return score;
 }
 
 void Solver::SolveAll()
 {
-    double total_performace = 0.;
+    unsigned total_score = 0;
     for (unsigned i = 1; i <= 186; ++i)
-        total_performace += Solve(i);
-    total_performace /= 186;
-    cout << "Final score: " << total_performace << endl;
+        total_score += Solve(i);
+    cout << "Final score: " << total_score << endl;
 }
--- src/cpp/solver.h
+++ src/cpp/solver.h
@@ -7,6 +7,6 @@ class Solver
 {
 public:
     static uint64_t Solve(const Matrix& m, Trace& output);
-    static double Solve(unsigned model_index);
+    static unsigned Solve(unsigned model_index);
     static void SolveAll();
 };
