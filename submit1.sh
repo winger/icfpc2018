00dad3f5e372949ff14c355d7ecd775e95d600dc
Den Raskovalov <denbox@gmail.com>
2018-07-20 19:51:10 -0700

    trivial submit scripts

---
+++ submit1.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd cppTracesL
+zip ../submit.zip *.nbt
---
+++ submit2.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+SHA=`sha256sum submit.zip | awk '{print $1}'`
+ID=`cat private.id`
+curl -L \
+    --data-urlencode action=submit \
+    --data-urlencode privateID=${ID} \
+    --data-urlencode submissionURL=$1 \
+    --data-urlencode submissionSHA=${SHA} \
+    https://script.google.com/macros/s/AKfycbzQ7Etsj7NXCN5thGthCvApancl5vni5SFsb1UoKgZQwTzXlrH7/exec
