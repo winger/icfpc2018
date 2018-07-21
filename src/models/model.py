c98c85d40c511b9b87a0bfc7b5e18c61d3e57de5
olegg <olegg@uber.com>
2018-07-20 22:32:03 -0700

    fix model parsing

--- src/models/model.py
+++ src/models/model.py
@@ -1,19 +1,18 @@
 import itertools
+import sys
 
-from bitstring import BitStream
+from bitarray import bitarray
 import numpy
 
 class Model:
     """Model file
 
     https://icfpcontest2018.github.io/lgtn/task-description.html#model-files"""
-    def __init__(self, stream):
-        self.size = stream.read("uint:8")
-        self.data = numpy.zeros((self.size, self.size, self.size))
-        for i in range(8, 8 + self.size ** 3, 8):
-            stream.reverse(i, i + 8)
-        for cell in itertools.product(range(self.size), repeat=3):
-            self.data[cell] = stream.read("bool")
+    def __init__(self, data):
+        size = bitarray(data[:8], endian="big")
+        self.size = int(size.tobytes()[0])
+        cells = numpy.frombuffer(data[8:].unpack(), dtype=bool).astype(int)
+        self.data = cells.reshape((self.size, self.size, self.size))
 
     def is_well_formed(self):
         """Validates model according to spec"""
@@ -23,5 +22,10 @@ class Model:
         return "Model size {}\nData {}".format(self.size, self.data)
 
 if __name__ == "__main__":
-    DEFAULT_MODEL = Model(BitStream(filename="problemsL/LA001_tgt.mdl"))
-    print(DEFAULT_MODEL)
+    if len(sys.argv) < 2:
+        sys.exit("No file to parse")
+    with open(sys.argv[1], "rb") as file:
+        data = bitarray(endian="little")
+        data.fromfile(file)
+        DEFAULT_MODEL = Model(data)
+        print(DEFAULT_MODEL)
