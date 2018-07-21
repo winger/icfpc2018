4bb7888c0b7692ce07036a0ecd8c5c60e22cd240
Dmitry Panin <pank.dm@gmail.com>
2018-07-21 07:49:59 -0700

    Show shasum of the file

--- submit2.sh
+++ submit2.sh
@@ -7,6 +7,7 @@ URL=${URL/drive.google.com\/open?id/drive.google.com\/uc?id}
 echo ${URL}
 FILE=$2
 SHA=`shasum -a 256 ${FILE} | awk '{print $1}'`
+shasum -a 256 ${FILE}
 ID=`cat private.id`
 curl -L \
     --data-urlencode action=submit \
