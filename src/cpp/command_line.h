26f6bda76d48a551acb328db98620ae4ae86c3c6
Den Raskovalov <denbox@gmail.com>
2018-07-21 19:48:16 -0700

    optionally run just one test

--- src/cpp/command_line.h
+++ src/cpp/command_line.h
@@ -1,14 +1,13 @@
 #pragma once
 
 #include <unordered_map>
-#include <map>
 #include <string>
 
 struct CommandLine {
     void Parse(int argc, char* const argv[]);
 
-    std::map<std::string, std::string> args;
-    std::map<std::string, int> int_args;
+    std::unordered_map<std::string, std::string> args;
+    std::unordered_map<std::string, int> int_args;
 };
 
 extern CommandLine cmd;
